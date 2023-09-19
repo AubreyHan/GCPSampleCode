3ec16452762c213a4794117295b5704b7de0bc14
Aubrey Han <aubreyhan@live.com>
2023-09-19 05:17:50 +0000

    Embedding

---
+++ AI/TextEmbedding.py
@@ -0,0 +1,12 @@
+from vertexai.language_models import TextEmbeddingModel
+
+
+def text_embedding() -> list:
+    """Text embedding with a Large Language Model."""
+    model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
+    embeddings = model.get_embeddings(["What is life?"])
+    for embedding in embeddings:
+        vector = embedding.values
+        print(f"Length of Embedding Vector: {len(vector)}")
+
+text_embedding()
